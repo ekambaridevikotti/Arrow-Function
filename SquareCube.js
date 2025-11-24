c0bba27d7a57e400688f1700b9fd325453963ddf
Ekambari Devi Kotti <ekambaridevi123@gmail.com>
2025-11-24 19:44:43 +0530

    Delete SquareCube.js

--- SquareCube.js
+++
@@ -1,9 +0,0 @@
-// L0 - Square and Cube of a Number with Implicit Return
-
-const calculateValues = num => ({
-  square: num * num,
-  cube: num * num * num
-});
-
-// Test Case
-console.log(calculateValues(5)); // Output: { square: 25, cube: 125 }
